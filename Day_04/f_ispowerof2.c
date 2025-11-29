344bbff28b5bc442d111e83da11a51f5cf44c404
Mohammed Ali <amaramohammedali42@gmail.com>
2025-11-29 19:02:58 +0100

    Enhance comments in f_ispowerof2 function
    Added comments to clarify the function's behavior regarding powers of 2.

--- Day_04/f_ispowerof2.c
+++ Day_04/f_ispowerof2.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+// unjustified but good try, when the parameter is 6 (x=6), it will return it as a puissance of 2, but actually!
+// 6 n'est pas une puissance de 2 frero :), by Ali
 int f_ispowerof2(int x) {
     if(x%2==0){
     	printf("%d est une puissance de 2",x);
